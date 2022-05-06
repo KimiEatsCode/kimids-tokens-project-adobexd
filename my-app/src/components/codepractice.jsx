59a1fd55bcfedc770a99e5cf65c6a47655487505
Kimi Rettig <sarahkmcaleer@gmail.com>
2022-05-06 14:31:42 -0400

    creating react app files starter; practicing react

---
+++ my-app/src/components/codepractice.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+
+// function formatName(user) {
+//     return user.firstName + ' ' + user.lastName;
+// };
+
+// const user = {
+//     firstName: 'Harper',
+//     lastName: 'Perez'
+// };
+
+// const element = (
+//     <h1>
+//         Hello, {formatName(user)}!
+//     </h1>
+// );
+
+// function getGreeting(user) {
+//     if(user) {
+//         return <h1>Hello, {formatName(user)}!</h1>
+//     }
+//     return <h1>Hello, Stranger.</h1>
+// }
+
+// By default, React DOM escapes any values embedded in JSX before rendering them. Thus it ensures that you can never inject anything that’s not explicitly written in your application. Everything is converted to a string before being rendered. This helps prevent XSS (cross-site-scripting) attacks.
+
